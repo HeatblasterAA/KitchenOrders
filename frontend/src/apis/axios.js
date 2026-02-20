98e3b65398b3669d31eb6232807d42408ef20857
HeatblasterAA <amanarora_60@yahoo.com>
2026-02-21 03:24:04 +0530

    set base url

--- frontend/src/apis/axios.js
+++ frontend/src/apis/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://kitchenorders-production.up.railway.app/",
 });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
